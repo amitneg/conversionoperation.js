c1e3c92df7df566d16ac7227ca44a260fad98e1a
amitneg <asnegi885@gmail.com>
2026-01-10 20:39:41 +0530

    conversionoperation.js

---
+++ javascript/conversionoperation.js
@@ -0,0 +1,8 @@
+// let someNumber=233
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let stringName ="amit"
+let nameString = Number(stringName)
+console.log(stringName);
